76d91b0ab41d18dfef56866f8caa04dc9e9d71ce
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2013-12-14 09:46:06 +0100

    lists-rotate-elixir: refactor to make correct use of the pipeline operator

--- lists-rotate-elixir/lib/lists_rotate_elixir.ex
+++ lists-rotate-elixir/lib/lists_rotate_elixir.ex
@@ -1,7 +1,7 @@
 defmodule ListsRotateElixir do
   import Enum, [:reverse, 1]
 
-  def lrotate(0, list), do: list
-  def lrotate(number, [head|list]), do: lrotate(number - 1, list ++ [head])
-  def rrotate(number, list), do: lrotate(number, list |> reverse) |> reverse
+  def lrotate(list, 0), do: list
+  def lrotate([head|list], number), do: lrotate(list ++ [head], number - 1)
+  def rrotate(list, number), do: list |> reverse |> lrotate(number) |> reverse
 end
--- lists-rotate-elixir/test/lists_rotate_elixir_test.exs
+++ lists-rotate-elixir/test/lists_rotate_elixir_test.exs
@@ -3,21 +3,21 @@ defmodule ListsRotateElixirTest do
 
   test "#lrotate, when k=3" do
     n7 = [:a, :b, :c, :d, :e, :f, :g]
-    assert ListsRotateElixir.lrotate(3, n7) == [:d, :e, :f, :g, :a, :b, :c]
+    assert ListsRotateElixir.lrotate(n7, 3) == [:d, :e, :f, :g, :a, :b, :c]
   end
 
   test "#rrotate, when k=3" do
     n7 = [:a, :b, :c, :d, :e, :f, :g]
-    assert ListsRotateElixir.rrotate(3, n7) == [:e, :f, :g, :a, :b, :c, :d]
+    assert ListsRotateElixir.rrotate(n7, 3) == [:e, :f, :g, :a, :b, :c, :d]
   end
 
   test "#rrotate, when k=7" do
     n7 = [:a, :b, :c, :d, :e, :f, :g]
-    assert ListsRotateElixir.rrotate(7, n7) == [:a, :b, :c, :d, :e, :f, :g]
+    assert ListsRotateElixir.rrotate(n7, 7) == [:a, :b, :c, :d, :e, :f, :g]
   end
 
   test "#rrotate, when k=8" do
     n7 = [:a, :b, :c, :d, :e, :f, :g]
-    assert ListsRotateElixir.rrotate(8, n7) == [:g, :a, :b, :c, :d, :e, :f]
+    assert ListsRotateElixir.rrotate(n7, 8) == [:g, :a, :b, :c, :d, :e, :f]
   end
 end
